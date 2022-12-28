c78bfa11b95335b5c1a5ed2710929e87a3424eee
Mujahidin1802 <mujahidin28394@gmail.com>
2022-12-28 14:51:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { NavbarComponent } from "./components";
+import { Home, Sukses } from "./pages";
+
+export default class App extends Component {
+  render() {
+    return (
+      // ...
+      <BrowserRouter>
+        <NavbarComponent />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sukses" element={<Sukses />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
